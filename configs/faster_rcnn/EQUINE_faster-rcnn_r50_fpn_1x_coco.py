c987916a042a1e0ea22bc5827868eb57c6b06ae1
ewernn <ewernn@users.noreply.github.com>
2024-03-02 21:42:53 +0000

    made simpler test_pipeline config

--- configs/faster_rcnn/EQUINE_faster-rcnn_r50_fpn_1x_coco.py
+++ configs/faster_rcnn/EQUINE_faster-rcnn_r50_fpn_1x_coco.py
@@ -1,42 +1,18 @@
 # Simplified Configuration for ONNX Export
-# model = dict(
-#     type='FasterRCNN',
-#     # Simplify the backbone, neck, and other components as required
-#     backbone=dict(
-#         type='ResNet',
-#         depth=50,
-#         num_stages=4,
-#         out_indices=(0, 1, 2, 3),
-#         frozen_stages=1,
-#         norm_cfg=dict(type='BN', requires_grad=False),
-#         norm_eval=True,
-#         style='pytorch',
-#         init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')
-#     ),
-#     neck=dict(
-#         type='FPN',
-#         in_channels=[256, 512, 1024, 2048],
-#         out_channels=256,
-#         num_outs=5
-#     ),
-#     # Specify roi_head, rpn_head, and other components as required
-# )
 model = dict(
+    type='FasterRCNN',
+    # Simplify the backbone, neck, and other components as required
     backbone=dict(
+        type='ResNet',
         depth=50,
-        frozen_stages=1,
-        init_cfg=dict(checkpoint='torchvision://resnet50', type='Pretrained'),
-        norm_cfg=dict(requires_grad=True, type='BN'),
-        norm_eval=True,
         num_stages=4,
-        out_indices=(
-            0,
-            1,
-            2,
-            3,
-        ),
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=1,
+        norm_cfg=dict(type='BN', requires_grad=False),
+        norm_eval=True,
         style='pytorch',
-        type='ResNet'),
+        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')
+    ),
     data_preprocessor=dict(
         bgr_to_rgb=True,
         mean=[
@@ -52,15 +28,12 @@ model = dict(
         ],
         type='DetDataPreprocessor'),
     neck=dict(
-        in_channels=[
-            256,
-            512,
-            1024,
-            2048,
-        ],
-        num_outs=5,
+        type='FPN',
+        in_channels=[256, 512, 1024, 2048],
         out_channels=256,
-        type='FPN'),
+        num_outs=5
+    ),
+    # Specify roi_head, rpn_head, and other components as required
     roi_head=dict(
         bbox_head=dict(
             bbox_coder=dict(
@@ -183,25 +156,37 @@ model = dict(
             max_per_img=1000,
             min_bbox_size=0,
             nms=dict(iou_threshold=0.7, type='nms'),
-            nms_pre=2000)),
-    type='FasterRCNN')
+            nms_pre=2000)))
 
-# Ensure the test pipeline is compatible and simple for ONNX export
+
 test_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(
-        type='MultiScaleFlipAug',
-        img_scale=(1333, 800),  # Image scale
-        flip=False,  # No flip for simplicity
-        transforms=[
-            dict(type='Resize', keep_ratio=True),
-            dict(type='Normalize', mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True),
-            dict(type='Pad', size_divisor=32),
-            dict(type='ImageToTensor', keys=['img']),
-            dict(type='Collect', keys=['img'])  # Ensure keys match expected values
-        ])
-    
+    dict(type='Resize', keep_ratio=True, scale=(1000, 1000)),  # If specific resizing is needed
+    dict(type='Normalize', mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True),
+    dict(type='Pad', size_divisor=32),
+    dict(type='ImageToTensor', keys=['img']),
+    #dict(type='Collect', keys=['img']),
+    dict(type='Collect', keys=['img']),
 ]
+
+
+# Ensure the test pipeline is compatible and simple for ONNX export
+# test_pipeline = [
+#     dict(type='LoadImageFromFile'),
+#     dict(
+#         type='MultiScaleFlipAug',
+#         scale_factor=(1333, 800),  # Image scale
+#         #flip=False,  # No flip for simplicity
+#         transforms=[
+#             dict(type='Resize', keep_ratio=True),
+#             dict(type='Normalize', mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True),
+#             dict(type='Pad', size_divisor=32),
+#             dict(type='ImageToTensor', keys=['img']),
+#             dict(type='Collect', keys=['img'])  # Ensure keys match expected values
+#         ])
+    
+# ]
+
 
 # Other configurations as required for ONNX export
 data = dict(
@@ -216,121 +201,9 @@ data = dict(
 # Specify other configurations as needed, such as dataset type, work directory, etc.
 
 
-
-
-
-
-
-
-
-
 #auto_scale_lr = dict(base_batch_size=16, enable=False)
 backend_args = None
 checkpoint_config = dict(interval=1)
-# data = dict(
-#     test=dict(
-#         ann_file='/home/eawern/EqNeckImagesSubset20/Data_coco.json',
-#         img_prefix='/home/eawern/EqNeckImagesSubset20/',
-#         pipeline=[
-#             dict(type='LoadImageFromFile'),
-#             dict(
-#                 transforms=[
-#                     dict(
-#                         mean=[
-#                             113.76166347104252,
-#                             113.76166347104252,
-#                             113.76166347104252,
-#                         ],
-#                         std=[
-#                             75.57327894366065,
-#                             75.57327894366065,
-#                             75.57327894366065,
-#                         ],
-#                         to_rgb=True,
-#                         type='Normalize'),
-#                     dict(size_divisor=32, type='Pad'),
-#                     dict(keys=[
-#                         'img',
-#                     ], type='ImageToTensor'),
-#                     dict(keys=[
-#                         'img',
-#                     ], type='Collect'),
-#                 ],
-#                 type='MultiScaleFlipAug'),
-#         ],
-#         type='CocoDataset'),
-#     train=dict(
-#         ann_file='/home/eawern/EqNeckImagesSubset20/Data_coco.json',
-#         img_prefix='/home/eawern/EqNeckImagesSubset20/',
-#         pipeline=[
-#             dict(to_float32=True, type='LoadImageFromFile'),
-#             dict(type='LoadAnnotations', with_bbox=True),
-#             dict(
-#                 max_rotate_degree=20,
-#                 scaling_ratio_range=(
-#                     0.8,
-#                     1.2,
-#                 ),
-#                 type='RandomAffine'),
-#             dict(
-#                 brightness_delta=32,
-#                 contrast_range=(
-#                     0.8,
-#                     1.2,
-#                 ),
-#                 saturation_range=(
-#                     0.8,
-#                     1.2,
-#                 ),
-#                 type='PhotoMetricDistortion'),
-#             dict(
-#                 mean=[
-#                     113.76166347104252,
-#                     113.76166347104252,
-#                     113.76166347104252,
-#                 ],
-#                 std=[
-#                     75.57327894366065,
-#                     75.57327894366065,
-#                     75.57327894366065,
-#                 ],
-#                 to_rgb=True,
-#                 type='Normalize'),
-#             dict(size_divisor=32, type='Pad'),
-#             dict(type='LoadAnnotations', with_bbox=True),
-#         ],
-#         type='CocoDataset'),
-#     val=dict(
-#         ann_file='/home/eawern/EqNeckImagesSubset20/Data_coco.json',
-#         img_prefix='/home/eawern/EqNeckImagesSubset20/',
-#         pipeline=[
-#             dict(type='LoadImageFromFile'),
-#             dict(
-#                 transforms=[
-#                     dict(
-#                         mean=[
-#                             113.76166347104252,
-#                             113.76166347104252,
-#                             113.76166347104252,
-#                         ],
-#                         std=[
-#                             75.57327894366065,
-#                             75.57327894366065,
-#                             75.57327894366065,
-#                         ],
-#                         to_rgb=True,
-#                         type='Normalize'),
-#                     dict(size_divisor=32, type='Pad'),
-#                     dict(keys=[
-#                         'img',
-#                     ], type='ImageToTensor'),
-#                     dict(keys=[
-#                         'img',
-#                     ], type='Collect'),
-#                 ],
-#                 type='MultiScaleFlipAug'),
-#         ],
-#         type='CocoDataset'))
 data_root = '/home/eawern/EqNeckImagesSubset20/'
 dataset_type = 'CocoDataset'
 default_hooks = dict(
@@ -372,170 +245,6 @@ lr_config = dict(
     warmup='linear',
     warmup_iters=500,
     warmup_ratio=0.001)
-# model = dict(
-#     backbone=dict(
-#         depth=50,
-#         frozen_stages=1,
-#         init_cfg=dict(checkpoint='torchvision://resnet50', type='Pretrained'),
-#         norm_cfg=dict(requires_grad=True, type='BN'),
-#         norm_eval=True,
-#         num_stages=4,
-#         out_indices=(
-#             0,
-#             1,
-#             2,
-#             3,
-#         ),
-#         style='pytorch',
-#         type='ResNet'),
-#     data_preprocessor=dict(
-#         bgr_to_rgb=True,
-#         mean=[
-#             123.675,
-#             116.28,
-#             103.53,
-#         ],
-#         pad_size_divisor=32,
-#         std=[
-#             58.395,
-#             57.12,
-#             57.375,
-#         ],
-#         type='DetDataPreprocessor'),
-#     neck=dict(
-#         in_channels=[
-#             256,
-#             512,
-#             1024,
-#             2048,
-#         ],
-#         num_outs=5,
-#         out_channels=256,
-#         type='FPN'),
-#     roi_head=dict(
-#         bbox_head=dict(
-#             bbox_coder=dict(
-#                 target_means=[
-#                     0.0,
-#                     0.0,
-#                     0.0,
-#                     0.0,
-#                 ],
-#                 target_stds=[
-#                     0.1,
-#                     0.1,
-#                     0.2,
-#                     0.2,
-#                 ],
-#                 type='DeltaXYWHBBoxCoder'),
-#             fc_out_channels=1024,
-#             in_channels=256,
-#             loss_bbox=dict(loss_weight=1.0, type='L1Loss'),
-#             loss_cls=dict(
-#                 loss_weight=1.0, type='CrossEntropyLoss', use_sigmoid=False),
-#             num_classes=2,
-#             reg_class_agnostic=False,
-#             roi_feat_size=7,
-#             type='Shared2FCBBoxHead'),
-#         bbox_roi_extractor=dict(
-#             featmap_strides=[
-#                 4,
-#                 8,
-#                 16,
-#                 32,
-#             ],
-#             out_channels=256,
-#             roi_layer=dict(output_size=7, sampling_ratio=0, type='RoIAlign'),
-#             type='SingleRoIExtractor'),
-#         type='StandardRoIHead'),
-#     rpn_head=dict(
-#         anchor_generator=dict(
-#             ratios=[
-#                 0.5,
-#                 1.0,
-#                 2.0,
-#             ],
-#             scales=[
-#                 8,
-#             ],
-#             strides=[
-#                 4,
-#                 8,
-#                 16,
-#                 32,
-#                 64,
-#             ],
-#             type='AnchorGenerator'),
-#         bbox_coder=dict(
-#             target_means=[
-#                 0.0,
-#                 0.0,
-#                 0.0,
-#                 0.0,
-#             ],
-#             target_stds=[
-#                 1.0,
-#                 1.0,
-#                 1.0,
-#                 1.0,
-#             ],
-#             type='DeltaXYWHBBoxCoder'),
-#         feat_channels=256,
-#         in_channels=256,
-#         loss_bbox=dict(loss_weight=1.0, type='L1Loss'),
-#         loss_cls=dict(
-#             loss_weight=1.0, type='CrossEntropyLoss', use_sigmoid=True),
-#         type='RPNHead'),
-#     test_cfg=dict(
-#         rcnn=dict(
-#             max_per_img=100,
-#             nms=dict(iou_threshold=0.5, type='nms'),
-#             score_thr=0.05),
-#         rpn=dict(
-#             max_per_img=1000,
-#             min_bbox_size=0,
-#             nms=dict(iou_threshold=0.7, type='nms'),
-#             nms_pre=1000)),
-#     train_cfg=dict(
-#         rcnn=dict(
-#             assigner=dict(
-#                 ignore_iof_thr=-1,
-#                 match_low_quality=False,
-#                 min_pos_iou=0.5,
-#                 neg_iou_thr=0.5,
-#                 pos_iou_thr=0.5,
-#                 type='MaxIoUAssigner'),
-#             debug=False,
-#             pos_weight=-1,
-#             sampler=dict(
-#                 add_gt_as_proposals=True,
-#                 neg_pos_ub=-1,
-#                 num=512,
-#                 pos_fraction=0.25,
-#                 type='RandomSampler')),
-#         rpn=dict(
-#             allowed_border=-1,
-#             assigner=dict(
-#                 ignore_iof_thr=-1,
-#                 match_low_quality=True,
-#                 min_pos_iou=0.3,
-#                 neg_iou_thr=0.3,
-#                 pos_iou_thr=0.7,
-#                 type='MaxIoUAssigner'),
-#             debug=False,
-#             pos_weight=-1,
-#             sampler=dict(
-#                 add_gt_as_proposals=False,
-#                 neg_pos_ub=-1,
-#                 num=256,
-#                 pos_fraction=0.5,
-#                 type='RandomSampler')),
-#         rpn_proposal=dict(
-#             max_per_img=1000,
-#             min_bbox_size=0,
-#             nms=dict(iou_threshold=0.7, type='nms'),
-#             nms_pre=2000)),
-#     type='FasterRCNN')
 optim_wrapper = dict(
     optimizer=dict(lr=0.02, momentum=0.9, type='SGD', weight_decay=0.0001),
     type='OptimWrapper')
@@ -765,3 +474,108 @@ val_evaluator = dict(
     type='CocoMetric')
 
 work_dir = '/home/eawern/'
+###############################################################################################################################
+# data = dict(
+#     test=dict(
+#         ann_file='/home/eawern/EqNeckImagesSubset20/Data_coco.json',
+#         img_prefix='/home/eawern/EqNeckImagesSubset20/',
+#         pipeline=[
+#             dict(type='LoadImageFromFile'),
+#             dict(
+#                 transforms=[
+#                     dict(
+#                         mean=[
+#                             113.76166347104252,
+#                             113.76166347104252,
+#                             113.76166347104252,
+#                         ],
+#                         std=[
+#                             75.57327894366065,
+#                             75.57327894366065,
+#                             75.57327894366065,
+#                         ],
+#                         to_rgb=True,
+#                         type='Normalize'),
+#                     dict(size_divisor=32, type='Pad'),
+#                     dict(keys=[
+#                         'img',
+#                     ], type='ImageToTensor'),
+#                     dict(keys=[
+#                         'img',
+#                     ], type='Collect'),
+#                 ],
+#                 type='MultiScaleFlipAug'),
+#         ],
+#         type='CocoDataset'),
+#     train=dict(
+#         ann_file='/home/eawern/EqNeckImagesSubset20/Data_coco.json',
+#         img_prefix='/home/eawern/EqNeckImagesSubset20/',
+#         pipeline=[
+#             dict(to_float32=True, type='LoadImageFromFile'),
+#             dict(type='LoadAnnotations', with_bbox=True),
+#             dict(
+#                 max_rotate_degree=20,
+#                 scaling_ratio_range=(
+#                     0.8,
+#                     1.2,
+#                 ),
+#                 type='RandomAffine'),
+#             dict(
+#                 brightness_delta=32,
+#                 contrast_range=(
+#                     0.8,
+#                     1.2,
+#                 ),
+#                 saturation_range=(
+#                     0.8,
+#                     1.2,
+#                 ),
+#                 type='PhotoMetricDistortion'),
+#             dict(
+#                 mean=[
+#                     113.76166347104252,
+#                     113.76166347104252,
+#                     113.76166347104252,
+#                 ],
+#                 std=[
+#                     75.57327894366065,
+#                     75.57327894366065,
+#                     75.57327894366065,
+#                 ],
+#                 to_rgb=True,
+#                 type='Normalize'),
+#             dict(size_divisor=32, type='Pad'),
+#             dict(type='LoadAnnotations', with_bbox=True),
+#         ],
+#         type='CocoDataset'),
+#     val=dict(
+#         ann_file='/home/eawern/EqNeckImagesSubset20/Data_coco.json',
+#         img_prefix='/home/eawern/EqNeckImagesSubset20/',
+#         pipeline=[
+#             dict(type='LoadImageFromFile'),
+#             dict(
+#                 transforms=[
+#                     dict(
+#                         mean=[
+#                             113.76166347104252,
+#                             113.76166347104252,
+#                             113.76166347104252,
+#                         ],
+#                         std=[
+#                             75.57327894366065,
+#                             75.57327894366065,
+#                             75.57327894366065,
+#                         ],
+#                         to_rgb=True,
+#                         type='Normalize'),
+#                     dict(size_divisor=32, type='Pad'),
+#                     dict(keys=[
+#                         'img',
+#                     ], type='ImageToTensor'),
+#                     dict(keys=[
+#                         'img',
+#                     ], type='Collect'),
+#                 ],
+#                 type='MultiScaleFlipAug'),
+#         ],
+#         type='CocoDataset'))
