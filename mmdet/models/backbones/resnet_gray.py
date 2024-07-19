3b1b9d365b1320bb6e8854567c6a8f73d31581e2
ewernn <eawern@outlook.com>
2024-07-19 09:18:24 -0700

    new configs (might not work)

--- mmdet/models/backbones/resnet_gray.py
+++ mmdet/models/backbones/resnet_gray.py
@@ -11,8 +11,8 @@ class ResNetGray(ResNet):
 
     def init_weights(self):
         super().init_weights()
-        # If there's a pretrained checkpoint, adjust the first conv layer
-        if self.init_cfg is not None and self.init_cfg['type'] == 'Pretrained':
+        # Check if init_cfg is a dict and has the 'type' key set to 'Pretrained'
+        if isinstance(self.init_cfg, dict) and self.init_cfg.get('type') == 'Pretrained':
             orig_conv1_weight = self.conv1.weight.data
             # Average the weights across the input channels and repeat for the single channel
             self.conv1.weight.data = orig_conv1_weight.mean(dim=1, keepdim=True).repeat(1, 1, 1, 1)
